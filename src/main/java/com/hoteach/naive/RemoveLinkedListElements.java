e898d15b472207c284f5eb35f856db08413e396e
hekai <kai.he02@ele.me>
2017-10-20 17:55:44 +0800

    BinaryTreeMaximumNode completed

--- src/main/java/com/hoteach/naive/RemoveLinkedListElements.java
+++ src/main/java/com/hoteach/naive/RemoveLinkedListElements.java
@@ -11,15 +11,13 @@ public class RemoveLinkedListElements {
 
 
     public ListNode removeElements(ListNode head, int val) {
-
         ListNode helper = new ListNode(0);
         helper.next = head;
         ListNode p = helper;
 
         while (p.next != null) {
             if (p.next.val == val) {
-                ListNode next = p.next;
-                p.next = next.next;
+                p.next = p.next.next;
             } else {
                 p = p.next;
             }
