c3aa358ba2d5d14935086830a7c07e4cdfbe3204
khe1314520@gmail.com <khe1314520@gmail.com>
2021-04-20 19:49:11 +0800

    非递归算法

--- src/main/java/com/hoteach/naive/CoinChange.java
+++ src/main/java/com/hoteach/naive/CoinChange.java
@@ -1,5 +1,8 @@
 package com.hoteach.naive;
 
+import com.google.common.collect.Lists;
+
+import javax.sound.midi.Soundbank;
 import java.util.*;
 
 /**
@@ -39,12 +42,61 @@ public class CoinChange {
     }
 
 
+    public int coinChange(List<Integer> coins, Integer money, Map<Integer, Integer> cache, List<Integer> path) {
+
+
+        Integer integer = cache.get(money);
+        if (Objects.nonNull(integer)) {
+            return integer;
+        }
+
+        if (money == 0) return 0;
+        if (money < 0) return -1;
+
+        int temp = Integer.MAX_VALUE;
+
+        for (Integer coin : coins) {
+            int result = coinChange(coins, money - coin, cache, path);
+            if (result == -1) {
+                continue;
+            }
+            if (result + 1 < temp) {
+                path.add(coin);
+                temp = result + 1;
+            }
+        }
+
+        temp = temp == Integer.MAX_VALUE ? -1 : temp;
+        cache.put(money, temp);
+        return temp;
+    }
+
+
+    public int coinChange(List<Integer> coins, Integer money) {
+        Integer[] dp = new Integer[money + 1];
+        Arrays.fill(dp, money + 1);
+
+
+        dp[0] = 0;
+        for (int m = 0; m < dp.length; m++) {
+            for (Integer coin : coins) {
+                if (m - coin < 0) {
+                    continue;
+                }
+                dp[m] = Math.min(dp[m], dp[m - coin] + 1);
+            }
+        }
+        return dp[money] == money + 1 ? -1 : dp[money];
+    }
+
+
     public static void main(String[] args) {
         CoinChange coinChange = new CoinChange();
         long l = System.currentTimeMillis();
-        int i = coinChange.coinChange(Arrays.asList(1, 3, 5, 6), 110, new HashMap<>());
-        System.out.println(System.currentTimeMillis() - l);
+        List<Integer> path = Lists.newArrayList();
+        int i = coinChange.coinChange(Arrays.asList(1, 3, 5, 6, 11), 11);
         System.out.println(i);
+        System.out.println(path);
     }
 
 }
