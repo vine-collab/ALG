e3500f21e62c1093cd0ab9ef9dde35a5f4edc866
khe1314520@gmail.com <khe1314520@gmail.com>
2021-05-12 20:08:10 +0800

    twosum

--- src/main/java/com/hoteach/naive/NSum.java
+++ src/main/java/com/hoteach/naive/NSum.java
@@ -1,6 +1,11 @@
 package com.hoteach.naive;
 
+import com.alibaba.fastjson.JSON;
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * nsum问题
@@ -30,10 +35,39 @@ public class NSum {
             if (sum < target) {
                 left++;
             }
-
         }
-
         return new int[]{};
+    }
+
+    public List<List<Integer>> twoSums(int[] nums, Integer target) {
+
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        int pointLeft = 0;
+        int pointRight = nums.length - 1;
+
+        while (pointLeft < pointRight) {
+            int left = nums[pointLeft];
+            int right = nums[pointRight];
+            int sum = nums[pointLeft] + nums[pointRight];
+            if (sum == target) {
+                result.add(Lists.newArrayList(nums[pointLeft], nums[pointRight]));
+                while (pointLeft < pointRight && nums[pointLeft] == left) {
+                    pointLeft++;
+                }
+                while (pointLeft < pointRight && nums[pointRight] == right) {
+                    pointRight--;
+                }
+            }
+            if (sum > target) {
+                pointRight--;
+            }
+
+            if (sum < target) {
+                pointLeft++;
+            }
+        }
+        return result;
     }
 
 
@@ -43,6 +77,13 @@ public class NSum {
         int target = 9;
         int[] ints = nSum.twoSum(nums, target);
         System.out.println(Arrays.toString(ints));
+        System.out.println("-------------------");
+        nums = new int[]{9, 8, 8, 8, 0};
+        target = 16;
+        List<List<Integer>> pairs = nSum.twoSums(nums, target);
+        System.out.println(JSON.toJSONString(pairs));
+
+
     }
 
 }
