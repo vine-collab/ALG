959e3780891df47af60ab9cd4b4cd0c7143411c0
khe1314520@gmail.com <khe1314520@gmail.com>
2021-05-19 15:30:52 +0800

    最长公共子序列

--- src/main/java/com/hoteach/naive/动态规划/LCS最长公共子序列.java
+++ src/main/java/com/hoteach/naive/动态规划/LCS最长公共子序列.java
@@ -24,14 +24,34 @@ public class LCS最长公共子序列 {
     }
 
 
+    int dp(String s1, String s2) {
+        // dp[i][j]的含义是：对于s1[1..i]和s2[1..j]，它们的 LCS 长度是dp[i][j]
+        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+        for (int i = 1; i < s1.length() + 1; i++) {
+            for (int j = 1; j < s2.length() + 1; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[s1.length()][s2.length()];
+    }
+
+
     public static void main(String[] args) {
         LCS最长公共子序列 c = new LCS最长公共子序列();
-        String s1 = "abc";
-        String s2 = "bxc";
+        String s1 = "abcde";
+        String s2 = "ace";
         int start1 = s1.length() - 1;
         int start2 = s2.length() - 1;
         int i = c.longestCommonSequence(s1, s2, start1, start2);
         System.out.println(i);
+        System.out.println("-----------------");
+        int dp = c.dp(s1, s2);
+        System.out.println(dp);
     }
 
 }
