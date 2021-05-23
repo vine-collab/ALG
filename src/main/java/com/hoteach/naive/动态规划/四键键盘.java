b88027f74964d473d71ffd07505df1293bdc0184
khe1314520@gmail.com <khe1314520@gmail.com>
2021-05-23 18:28:01 +0800

    四键盘问题

---
+++ src/main/java/com/hoteach/naive/动态规划/四键键盘.java
@@ -0,0 +1,21 @@
+package com.hoteach.naive.动态规划;
+
+/**
+ * @author 阿季
+ * @date 2021-05-23 18:26
+ */
+
+public class 四键键盘 {
+
+
+    int maxA(int n) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        四键键盘 c = new 四键键盘();
+        int i = c.maxA(5);
+        System.out.println(i);
+    }
+}
