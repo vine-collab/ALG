56a6acb5dd5edee9f6b80ec2235e0101773616ca
khe1314520@gmail.com <khe1314520@gmail.com>
2021-05-23 18:38:19 +0800

    四键盘

--- src/main/java/com/hoteach/naive/动态规划/四键键盘.java
+++ src/main/java/com/hoteach/naive/动态规划/四键键盘.java
@@ -9,13 +9,37 @@ public class 四键键盘 {
 
 
     int maxA(int n) {
-
-        return 0;
+        return dp(n, 0, 0);
     }
+
+    /**
+     * 状态3中:按了a,按了ctrl+v,按了 ctrl+a ctrl+c
+     * 选择：a, ctrl+a, ctrl+c, ctrl+v
+     * @param n
+     * @param a
+     * @param copy
+     * @return
+     */
+    int dp(int n, int a, int copy) {
+        if (n <= 0) {
+            return a;
+        }
+
+        // 按了a
+        int dp = dp(n - 1, a + 1, copy);
+        // 按了ctrl+v
+        int dp1 = dp(n - 1, a + copy, copy);
+
+        // 按了 ctrl+a ctrl+c
+        int dp2 = dp(n - 2, a, a);
+        return Math.max(Math.max(dp, dp1), dp2);
+
+    }
+
 
     public static void main(String[] args) {
         四键键盘 c = new 四键键盘();
-        int i = c.maxA(5);
+        int i = c.maxA(7);
         System.out.println(i);
     }
 }
