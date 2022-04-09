4f79c430fce629bb12b03f77b7b9d3f72693b475
Bob <>
2022-04-09 16:17:25 +0800

    test

--- src/main/java/com/vine/alg/BFS算法套路框架/开密码锁最少次数.java
+++ src/main/java/com/vine/alg/BFS算法套路框架/开密码锁最少次数.java
@@ -15,9 +15,12 @@ public class 开密码锁最少次数 {
 
     public static void main(String[] args) {
         开密码锁最少次数 k = new 开密码锁最少次数();
-        int i = k.openLock(new String[]{"1008", "1000"}, "0009");
+        int i = k.openLock(new String[]{"1008", "1000"}, "0089");
         System.out.println(i);
+        int i1 = k.openLock1(new String[]{"1008", "1000"}, "0089");
+        System.out.println(i1);
     }
+
 
     int openLock(String[] deadends, String target) {
 
@@ -35,7 +38,7 @@ public class 开密码锁最少次数 {
                 if (dead.contains(cur)) {
                     continue;
                 }
-                if (cur.equals(target)) {
+                if(cur.equals(target)) {
                     return step;
                 }
 
@@ -62,9 +65,57 @@ public class 开密码锁最少次数 {
     }
 
 
+    /**
+     * 将visited和dead合并
+     * @param deadends
+     * @param target
+     * @return
+     */
+    int openLock1(String[] deadends, String target) {
+
+        Queue<String> queue = new ArrayDeque<>();
+        Set<String> visited = Sets.newHashSet(deadends);
+        queue.offer("0000");
+        visited.add("0000");
+        int step = 0;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                String cur = queue.poll();
+
+                if(cur.equals(target)) {
+                    return step;
+                }
+
+
+                for (int j = 0; j < cur.length(); j++) {
+                    String plus = plus(cur, j);
+                    if (!visited.contains(plus)) {
+                        queue.offer(plus);
+                        visited.add(plus);
+                    }
+                }
+                for (int j = 0; j < cur.length(); j++) {
+                    String minus = minus(cur, j);
+                    if (!visited.contains(minus)) {
+                        queue.offer(minus);
+                        visited.add(minus);
+                    }
+                }
+
+            }
+            step++;
+        }
+        return -1;
+    }
+
+
+
+
     String plus(String s, int pos) {
         char[] chars = s.toCharArray();
-        if (chars[pos] == '9') {
+        if(chars[pos] == '9') {
             chars[pos] = '0';
         } else {
             chars[pos] += 1;
@@ -76,7 +127,7 @@ public class 开密码锁最少次数 {
 
     String minus(String s, int pos) {
         char[] chars = s.toCharArray();
-        if (chars[pos] == '0') {
+        if(chars[pos] == '0') {
             chars[pos] = '9';
         } else {
             chars[pos] -= 1;
