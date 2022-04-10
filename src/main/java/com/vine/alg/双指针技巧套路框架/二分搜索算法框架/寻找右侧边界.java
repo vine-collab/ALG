a47bfc2fc3a6a91e22bb543c63ad783927bfb50c
Bob <>
2022-04-10 16:46:26 +0800

    test

--- src/main/java/com/vine/alg/双指针技巧套路框架/二分搜索算法框架/寻找右侧边界.java
+++ src/main/java/com/vine/alg/双指针技巧套路框架/二分搜索算法框架/寻找右侧边界.java
@@ -1,83 +1,80 @@
 package com.vine.alg.双指针技巧套路框架.二分搜索算法框架;
 
-import java.lang.annotation.Target;
-
 /**
  * @author 阿季
  * @date 2022-04-10 3:45 PM
  */
 
-public class 寻找左侧边界 {
+public class 寻找右侧边界 {
     public static void main(String[] args) {
-        寻找左侧边界 x = new 寻找左侧边界();
+        寻找右侧边界 x = new 寻找右侧边界();
         int[] nums = {1, 2, 2, 4, 11, 15};
-        int i = x.searchLeftBound(nums, 5);
+        int i = x.searchRightBound(nums, 15);
         System.out.println(i);
 
-        int i1 = x.leftBound(nums, 2);
-        System.out.println(i1);
 
-        int i2 = x.searchLeftBound2(nums, 2);
+
+        int i2 = x.searchRightBound2(nums, 15);
         System.out.println(i2);
 
     }
 
     /*
-        返回结果可以理解为：代表数组中小于 target 的数有多少个
+        返回结果可以理解为：代表数组中大于于 target 的数有多少个
      */
-    int leftBound(int[] nums, int target) {
+    int rightBound(int[] nums, int target) {
 
         int left = 0;
         int right = nums.length;
         while (left < right) { // 注意：搜索区间[left, right)
             int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
-                right = mid; // 可以搜索左侧边界
+                left = mid + 1; // [mid + 1, right) , 由于 mid + 1 所以可以搜索右边界
             } else if (nums[mid] < target) {
-                left = mid + 1;
+                left = mid + 1; // [mid + 1, right)
             } else if (nums[mid] > target) {
-                right = mid; // 注意
+                right = mid; // [left, mid)
             }
         }
-        return left;
+        return left - 1; // 注意：因为 left = mid + 1
     }
 
 
-    int leftBound2(int[] nums, int target) {
+    int rightBound2(int[] nums, int target) {
 
         int left = 0;
         int right = nums.length - 1;
         while (left <= right) { // 注意：搜索区间[left, right]
             int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
-                right = mid - 1; // 收缩右边界
+                left = mid + 1; // 收缩左边界 [mid + 1, right]
             } else if (nums[mid] < target) {
                 left = mid + 1;  // 搜索区间[mid + 1, right]
             } else if (nums[mid] > target) {
                 right = mid - 1; // 搜索区间[left, right - 1]
             }
         }
-        return left;
+        return right;
     }
 
-    int searchLeftBound2(int[] nums, int target) {
-        int i = leftBound2(nums, target);
-        if (i >= nums.length) {
+    int searchRightBound2(int[] nums, int target) {
+        int rightBound = rightBound2(nums, target);
+        if (rightBound < 0) {
             return -1;
         }
 
-        return nums[i] == target ? i : -1;
+        return nums[rightBound] == target ? rightBound : -1;
 
     }
 
 
-    int searchLeftBound(int[] nums, int target) {
-        int i = leftBound(nums, target);
-        if (i == nums.length) {
+    int searchRightBound(int[] nums, int target) {
+        int rightBound = rightBound(nums, target);
+        if (rightBound < 0) {
             return -1;
         }
 
-        return nums[i] == target ? i : -1;
+        return nums[rightBound] == target ? rightBound : -1;
 
     }
 
