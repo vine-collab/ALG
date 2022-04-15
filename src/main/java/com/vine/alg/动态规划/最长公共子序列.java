964e25d4d99aa9a0381da7382cc12e3a400c933d
Bob <>
2022-04-15 08:52:39 +0800

    test

--- src/main/java/com/vine/alg/动态规划/最长公共子序列.java
+++ src/main/java/com/vine/alg/动态规划/最长公共子序列.java
@@ -63,7 +63,7 @@ public class 最长公共子序列 {
 
 
     int dp(String s1, String s2) {
-        int dp[][] = new int[s1.length() + 1][s2.length() + 1];
+        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
         for (int i = 1; i <= s1.length(); i++) {
             for (int j = 1; j <= s2.length(); j++) {
                 if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
