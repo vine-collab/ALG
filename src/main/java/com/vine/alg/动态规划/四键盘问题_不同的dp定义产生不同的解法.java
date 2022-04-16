3d119e8f13dc2ff4445f403a689f94ae172d96aa
Bob <>
2022-04-16 18:28:01 +0800

    test

---
+++ src/main/java/com/vine/alg/动态规划/四键盘问题_不同的dp定义产生不同的解法.java
@@ -0,0 +1,24 @@
+package com.vine.alg.动态规划;
+
+/**
+ * @author 阿季
+ * @date 2022-04-16 6:21 PM
+ */
+
+public class 四键盘问题_不同的dp定义产生不同的解法 {
+    public static void main(String[] args) {
+        try {
+            四键盘问题_不同的dp定义产生不同的解法 z = new 四键盘问题_不同的dp定义产生不同的解法();
+            int i = z.maxA(5);
+            System.out.println(i);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    int maxA(int N) {
+        return 0;
+    }
+
+}
