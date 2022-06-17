8ebc333d7de6c858d5a4440a635879500319b049
Bob <>
2022-06-17 23:00:43 +0800

    test

---
+++ src/main/java/com/vine/alg/leet/leetcode/$583_两个字符串的删除操作.java
@@ -0,0 +1,37 @@
+package com.vine.alg.leet.leetcode;
+
+/**
+ * @author 阿季
+ * @date 2022-06-17 8:57 PM
+ */
+
+public class $583_两个字符串的删除操作 {
+
+
+    /**
+     * https://leetcode.cn/problems/delete-operation-for-two-strings/
+     *
+     * 给定两个单词word1和word2，返回使得word1和word2相同所需的最小步数。
+     *
+     * 每步可以删除任意一个字符串中的一个字符。
+     *
+     * 示例 1：
+     *
+     * 输入: word1 = "sea", word2 = "eat"
+     * 输出: 2
+     * 解释: 第一步将 "sea" 变为 "ea" ，第二步将 "eat "变为 "ea"
+     *
+     * 示例 2:
+     * 输入：word1 = "leetcode", word2 = "etco"
+     * 输出：4
+     *
+     */
+    public static void main(String[] args) {
+        $583_两个字符串的删除操作 l = new $583_两个字符串的删除操作();
+    }
+
+    public int minDistance(String word1, String word2) {
+
+    }
+
+}
