cb810a6cb2c1d19b2666621ef0b7963d612aeba5
Bob <>
2022-06-19 17:06:18 +0800

    test

--- src/main/java/com/vine/alg/leet/leetcode/$78_子集.java
+++ src/main/java/com/vine/alg/leet/leetcode/$78_子集.java
@@ -18,15 +18,15 @@ public class $78_子集 {
      * https://leetcode.cn/problems/subsets/
      * 78. 子集
      * 给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
-     *
+     * <p>
      * 解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。
-     *
+     * <p>
      * 示例 1：
-     *
+     * <p>
      * 输入：nums = [1,2,3]
      * 输出：[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
      * 示例 2：
-     *
+     * <p>
      * 输入：nums = [0]
      * 输出：[[],[0]]
      */
@@ -46,9 +46,7 @@ public class $78_子集 {
 
     void backtrack(int[] nums, int pos, LinkedList<Integer> cur) {
 
-        if (!contains(cur)) {
-            res.add(new ArrayList<>(cur));
-        }
+        res.add(new ArrayList<>(cur));
 
         for (int i = pos; i < nums.length; i++) {
             if (cur.contains(nums[i])) {
@@ -64,7 +62,7 @@ public class $78_子集 {
 
     boolean contains(List<Integer> cur) {
         for (List<Integer> re : res) {
-            if(re.size() != cur.size()) {
+            if (re.size() != cur.size()) {
                 continue;
             }
             return res.contains(cur);
