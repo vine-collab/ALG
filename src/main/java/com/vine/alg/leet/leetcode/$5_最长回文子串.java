2e26fe07b622643a9f0a833a8599fdaa5985ba9c
Bob <>
2022-06-23 20:40:10 +0800

    test

---
+++ src/main/java/com/vine/alg/leet/leetcode/$5_最长回文子串.java
@@ -0,0 +1,52 @@
+package com.vine.alg.leet.leetcode;
+
+/**
+ * @author 阿季
+ * @date 2022-06-22 8:30 PM
+ */
+
+public class $5_最长回文子串 {
+    public static void main(String[] args) {
+        $5_最长回文子串 z = new $5_最长回文子串();
+        String babad = z.longestPalindrome("babd");
+        System.out.println(babad);
+    }
+
+
+    /**
+     * dp：ij开头和结尾的是回文
+     * base case
+     * 1、单个字符，对角线为true
+     * <p>
+     * 递推重点(0, n - 1)
+     */
+    public String longestPalindrome(String s) {
+
+        int n = s.length(), start = 0, len = 1;
+        boolean[][] dp = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                if (s.charAt(i) != s.charAt(j)) {
+                    dp[i][j] = false;
+                } else {
+                    if (j - i < 3) { // 两个字符，或者两个字符中间夹一个字符如"aba"
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                }
+                if(!dp[i][j]) {
+                    continue;
+                }
+                if (j - i + 1 > len) {
+                    len = j - i + 1;
+                    start = i;
+                }
+            }
+        }
+        return s.substring(start, start + len);
+    }
+}
