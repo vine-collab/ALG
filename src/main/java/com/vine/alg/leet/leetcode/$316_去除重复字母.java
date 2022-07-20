fbd998e9718ecfd23c0d44d30af7f466e7483102
Bob <>
2022-07-20 22:04:31 +0800

    test

---
+++ src/main/java/com/vine/alg/leet/leetcode/$316_去除重复字母.java
@@ -0,0 +1,129 @@
+package com.vine.alg.leet.leetcode;
+
+import java.util.Stack;
+
+/**
+ * @author 阿季
+ * @date 2022-07-20 9:19 PM
+ */
+
+public class $316_去除重复字母 {
+
+
+    public static void main(String[] args) {
+        $316_去除重复字母 q = new $316_去除重复字母();
+        String bcabc = q.removeDuplicateLetters("bcabc");
+        System.out.println(bcabc);
+
+    }
+
+    String removeDuplicateLetters(String s) {
+        // 存放去重的结果
+        Stack<Character> stack = new Stack<>();
+        // 记录栈中是否存放过某个字符
+        boolean[] inStack = new boolean[256];
+
+        // 记录每个元素的个数
+        int[] count = new int[256];
+        for (char c : s.toCharArray()) {
+            count[c]++;
+        }
+
+        for (char c : s.toCharArray()) {
+            count[c]--;
+            if (inStack[c]) {
+                continue;
+            }
+
+            // 插入之前，和之前的元素比较一下大小
+            // 如果字典序比前面的小，pop前面的元素
+            while (!stack.isEmpty() && stack.peek() > c) {
+                // 后面已经没有栈顶的元素了，不能再pop了
+                if (count[stack.peek()] == 0) {
+                    break;
+                }
+                // 弹出栈顶元素
+                inStack[stack.pop()] = false;
+            }
+
+            stack.push(c);
+            inStack[c] = true;
+        }
+
+
+        StringBuilder sb = new StringBuilder();
+        while (!stack.empty()) {
+            sb.append(stack.pop());
+        }
+        // 栈中元素插入顺序是反的，需要 reverse 一下
+        return sb.reverse().toString();
+
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/vine/alg/前缀树/TrieMap.java
@@ -0,0 +1,55 @@
+package com.vine.alg.前缀树;
+
+public class TrieMap<V> {
+    // ASCII 码个数
+    private static final int R = 256;
+    // 当前存在 Map 中的键值对个数
+    private int size = 0;
+
+    private static class TrieNode<V> {
+        V val = null;
+        TrieNode<V>[] children = new TrieNode[R];
+    }
+
+    // Trie 树的根节点
+    private TrieNode<V> root = null;
+
+    /* 其他 API 的实现... */
+
+    public int size() {
+        return size;
+    }
+
+
+    // 从节点 node 开始搜索 key，如果存在返回对应节点，否则返回 null
+    private TrieNode<V> getNode(TrieNode<V> node, String key) {
+        TrieNode<V> p = node;
+        // 从节点 node 开始搜索 key
+        for (int i = 0; i < key.length(); i++) {
+            if (p == null) {
+                // 无法向下搜索
+                return null;
+            }
+            // 向下搜索
+            char c = key.charAt(i);
+            p = p.children[c];
+        }
+        return p;
+    }
+
+    // 搜索 key 对应的值，不存在则返回 null
+    public V get(String key) {
+        // 从 root 开始搜索 key
+        TrieNode<V> x = this.getNode(root, key);
+        if (x == null || x.val == null) {
+            // x 为空或 x 的 val 字段为空都说明 key 没有对应的值
+            return null;
+        }
+        return x.val;
+    }
+
+    // 判断 key 是否存在在 Map 中
+    public boolean containsKey(String key) {
+        return this.get(key) != null;
+    }
+}
