dd77f8339de7f50f2dd1ba73e952088bb3c0e98c
Bob <>
2022-07-23 17:56:18 +0800

    test

--- src/main/java/com/vine/alg/leet/leetcode/$714_买卖股票的最佳时机含手续费.java
+++ src/main/java/com/vine/alg/leet/leetcode/$714_买卖股票的最佳时机含手续费.java
@@ -45,12 +45,12 @@ public class $714_买卖股票的最佳时机含手续费 {
 
             if(i - 1 < 0) {
                 dp[i][0] = 0;
-                dp[i][1] = -prices[i];
+                dp[i][1] = -prices[i] - fee;
                 continue;
             }
 
-            dp[i][0] = Integer.max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee);
-            dp[i][1] = Integer.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+            dp[i][0] = Integer.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Integer.max(dp[i - 1][1], dp[i - 1][0] - prices[i]  - fee);
         }
         return dp[prices.length - 1][0];
     }
