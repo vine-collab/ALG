8f2787040351c60768e92d6b0249917c0815bbb5
Bob <>
2022-07-24 21:08:21 +0800

    test

--- src/main/java/com/vine/alg/leet/leetcode/$42_接雨水.java
+++ src/main/java/com/vine/alg/leet/leetcode/$42_接雨水.java
@@ -18,11 +18,49 @@ public class $42_接雨水 {
     }
 
     public int trap(int[] height) {
-
-        return 0;
+        int n = height.length;
+        int res = 0;
+        for (int i = 1; i < n - 1; i++) {
+            int l_max = 0, r_max = 0;
+            // 找右边最高的柱子
+            for (int j = i; j < n; j++)
+                r_max = Math.max(r_max, height[j]);
+            // 找左边最高的柱子
+            for (int j = i; j >= 0; j--)
+                l_max = Math.max(l_max, height[j]);
+            // 如果自己就是最高的话，
+            // l_max == r_max == height[i]
+            res += Math.min(l_max, r_max) - height[i];
+        }
+        return res;
     }
 
 
+    public int trapMemo(int[] height) {
+        if (height.length == 0) {
+            return 0;
+        }
+        int n = height.length;
+        int res = 0;
+        int[] leftMax = new int[n];
+        int[] rightMax = new int[n];
+        leftMax[0] = height[0];
+        rightMax[n - 1] = height[n - 1];
+        // 计算左侧最大值
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Integer.max(height[i], leftMax[i - 1]);
+        }
+        // 计算右侧最大值
+        for (int j = n - 2; j >= 0; j--) {
+            rightMax[j] = Integer.max(rightMax[j + 1], height[j]);
+        }
+
+        for (int i = 1; i < n; i++) {
+            res += Integer.min(leftMax[i], rightMax[i]) - height[i];
+        }
+        return res;
+
+    }
 }
 
 
