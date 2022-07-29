e75e195707357f5a6ac46878116813ebd72ef0fa
Bob <>
2022-07-29 20:28:40 +0800

    test

---
+++ src/main/java/com/vine/alg/leet/leetcode/$124_二叉树中的最大路径和.java
@@ -0,0 +1,110 @@
+package com.vine.alg.leet.leetcode;
+
+import com.vine.alg.基本数据结构构造.TreeNode;
+
+/**
+ * @author 阿季
+ * @date 2022-07-28 9:59 PM
+ */
+
+public class $124_二叉树中的最大路径和 {
+
+
+    /**
+     * https://leetcode.cn/problems/binary-tree-maximum-path-sum/
+     */
+    public static void main(String[] args) {
+        $124_二叉树中的最大路径和 e = new $124_二叉树中的最大路径和();
+    }
+
+    int res = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        // 计算单边路径和时顺便计算最大路径和
+        oneSideMax(root);
+        return res;
+    }
+
+    // 定义：计算从根节点 root 为起点的最大单边路径和
+    int oneSideMax(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftMaxSum = Math.max(0, oneSideMax(root.left));
+        int rightMaxSum = Math.max(0, oneSideMax(root.right));
+        // 后序遍历位置，顺便更新最大路径和
+        int pathMaxSum = root.val + leftMaxSum + rightMaxSum;
+        res = Math.max(res, pathMaxSum);
+        // 实现函数定义，左右子树的最大单边路径和加上根节点的值
+        // 就是从根节点 root 为起点的最大单边路径和
+        return Math.max(leftMaxSum, rightMaxSum) + root.val;
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
