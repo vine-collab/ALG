84c4fcee1b72ddb5bf505b476dadd51c204f90d2
Bob <>
2022-08-31 22:48:00 +0800

    test

--- src/main/java/com/vine/alg/code/leetcode/$179_最大数.java
+++ src/main/java/com/vine/alg/code/leetcode/$179_最大数.java
@@ -1,5 +1,9 @@
 package com.vine.alg.code.leetcode;
 
+import com.vine.alg.基本数据结构构造.Utils;
+
+import java.util.PriorityQueue;
+
 /**
  * @author 阿季
  * @date 2022-08-30 8:28 PM
@@ -25,9 +29,25 @@ public class $179_最大数 {
     public static void main(String[] args) {
 
         $179_最大数 z = new $179_最大数();
+        String s = z.largestNumber(Utils.splitToIntArr1("3,30,34,5,9"));
+        System.out.println(s);
+
     }
 
     public String largestNumber(int[] nums) {
+        PriorityQueue<String> queue = new PriorityQueue<>((x, y) -> (y + x).compareTo(x + y));
+        for (int num : nums) {
+            queue.offer(String.valueOf(num));
+        }
+
+        StringBuilder res = new StringBuilder();
+        while (queue.size() > 0) {
+            res.append(queue.poll());
+        }
+        if (res.charAt(0) == '0') {
+            return "0";
+        }
+        return res.toString();
 
     }
 }
